7dddb12372cffa5d51e3ec9a3b6159a883592071
Thenlie <leithen113@gmail.com>
2023-08-06 18:25:38 -0600

    style: add spacing to skeleton loader

--- src/components/RowLoading.tsx
+++ src/components/RowLoading.tsx
@@ -1,10 +1,10 @@
 const RowLoading = (): JSX.Element => (
   <div className="content h-full animate-pulse">
-    <div className="w-1/2">
+    <div className="w-1/2 py-1">
       <div className="h-[1em] w-full bg-[#dddddd]" />
     </div>
     <div className="w-2/12">
-      <div className="h-[0.7em] w-full bg-[#dddddd]" />
+      <div className="mb-1 h-[0.7em] w-full bg-[#dddddd]" />
       <div className="h-[0.7em] w-full bg-[#dddddd]" />
     </div>
   </div>
